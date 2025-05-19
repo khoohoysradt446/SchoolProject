d991f8685384e57b088c820cee328ba7bacff94f
khoohoysradt446 <khoohoysradt446@gmail.com>
2025-05-19 22:37:50 +0000

    Add 35.cpp

---
+++ src/35.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    string z = "Hello, World!";
+    cout << "x: " << x << ", y: " << y << ", z: " << z << endl;
+    return 0;
+}
